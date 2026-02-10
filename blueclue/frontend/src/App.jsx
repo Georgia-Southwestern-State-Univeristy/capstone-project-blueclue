9aac51410cd4716b4008b67c5bf223cf863efca4
tnewc3 <157632357+tnewc3@users.noreply.github.com>
2026-02-10 16:08:43 -0500

    Add routes for Login, Register, and Change Password pages in App component

--- blueclue/frontend/src/App.jsx
+++ blueclue/frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ChangePassword from './pages/ChangePassword'
 import ClientDashboard from './pages/ClientDashboard'
 import TechnicianDashboard from './pages/TechnicianDashboard'
 import Navbar from './components/Navbar'
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/change-password" element={<ChangePassword />} />
         <Route path="/client-dashboard" element={<ClientDashboard />} />
         <Route path="/technician" element={<TechnicianDashboard />} />
       </Routes>
